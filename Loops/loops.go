1e3a85e7779c4883a95f9bb6ffae02a80f6670c0
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-12-07 12:07:30 +0300

    go structs added to repository

--- Loops/loops.go
+++ Loops/loops.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -14,8 +14,48 @@ func main() {
 		fmt.Println("index %d value: %d \n",i,value)
 	}
 
-	for i := 0; i<10 ;i++ {
-		slc_2 =append(slc_2,1)
-	}
+	//for i := 0; i<10; i++ {
+	//	slc_2 =append(slc_2,1)
+//	}
 
+for i := range [10]int{}{
+	slc_2 =append(slc_2,i)
+}
+
+for _, value_2 := range slc_2 {
+	for _, value_1 := range slc_1 {
+		value_1 +=value_2
+	}
+}
+
+for _, value2 := range slc_2 {
+	for i := range slc_1 {
+		slc_1[i] += value2
+	}
+}
+
+
+c := time.After(5*time.Second)
+
+for{
+	b := false
+	select {
+	case <-c:
+		b= true
+	default:
+		fmt.Println(time.Now())
+		time.Sleep(1*time.Second)
+	}
+	if b{
+		break
+	}
+}
+fmt.Println("test")
+
+
+
+	fmt.Println(slc_1)
+	fmt.Println(slc_2)
+	fmt.Println("slc1 len and cap",len(slc_1),cap(slc_1))
+	fmt.Println("slc2 len and cap",len(slc_2),cap(slc_2))
 }
---
+++ Structs/Structs.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	// goda buit-in gelen kütüphane
+	"time"
+)
+
+type Kullanıcı struct {
+	Ad      string      `json:"adi"` // tag olarak tanımlanıyor
+	Soyad   string      `json:"soyad"`
+	Takipci []Kullanıcı `json:"takipci,omitempty"`
+	//Begeni [] struct {
+	//Tarih time.Time
+	//}
+	Begeni []Begen
+}
+
+type Begen struct {
+	Tarih time.Time
+}
+
+func main() {
+	k := Kullanıcı{Ad: "Emirhan", Soyad: "Doğandemir", Takipci: []Kullanıcı{
+		{Ad: "Hasan", Soyad: "huseyin"}, {Ad: "Mehmet", Soyad: "Erbakan"},
+	}}
+	fmt.Println(k.Ad)
+	arr, _ := json.Marshal(k) // bir structun json çıktısını almak istiyorsak bu paketi kullanmalıyız
+	fmt.Println(string(arr))
+	fmt.Println(k)
+}
